87735a6932a3d2132b7156ccfa5b25e5cb1418ea
Ron Smeral <ron.smeral@gmail.com>
2020-02-13 23:10:14 +0100

    add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    sourceType: 'module'
+  },
+  rules: {
+    "max-len": ['warn', 120]
+  }
+};
---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "printWidth": 120,
+  "trailingComma": "none",
+  "tabWidth": 2,
+  "semi": true,
+  "singleQuote": true,
+  "quoteProps": "as-needed",
+  "bracketSpacing": false
+}
