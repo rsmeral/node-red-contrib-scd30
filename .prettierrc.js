0978f03f4c12fff659a8e8512a5fec6403cf439e
Ron Smeral <ron@smeral.net>
2020-02-14 23:19:55 +0100

    remove prettier and TS

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  "printWidth": 120,
-  "trailingComma": "none",
-  "tabWidth": 2,
-  "semi": true,
-  "singleQuote": true,
-  "quoteProps": "as-needed",
-  "bracketSpacing": false
-}
